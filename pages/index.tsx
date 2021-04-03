7791c10ac1f1fbb3dbef5b10c8167cef7cbdb19f
pedrogodoy <pedrogh.godoy12@gmail.com>
2021-04-03 16:44:51 -0400

    add react-animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/Layout'
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useDebounce } from 'use-lodash-debounce';
 import { toast, ToastContainer } from 'react-nextjs-toast';
 import { IClaps } from '../types';
 import { InferGetServerSidePropsType } from 'next';
 import UseDebounce from '../components/UseDebounce';
-
+import { pulse, fadeIn } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
 
 export default function IndexPage({ 
   claps }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [clapData, setClapData] = React.useState(claps);
-  const [showCounter, setShowCounter] = React.useState('none');
+  const [showCounter, setShowCounter] = React.useState('hidden');
+  const [animate, setAnimate] = React.useState(false);
   const debouncedValue = UseDebounce(clapData.claps, 1000);
 
   useEffect(() => {
@@ -21,17 +22,17 @@ export default function IndexPage({
   }, [debouncedValue])
 
   const handleClaps = async () => {
+    animate ? setAnimate(false) : setAnimate(true)
+
     if(clapData.claps < 50) {
       setClapData({...clapData, claps: clapData.claps + 1 });
-      setShowCounter('flex');
+      setShowCounter('visible');
     }
   };
 
 
   const makeRequest = async () => {
     try {
-      console.log('chamou');
-
       const res = await fetch('http://localhost:3333/articles/claps', {
         body: JSON.stringify({
           claps: clapData?.claps
@@ -43,7 +44,7 @@ export default function IndexPage({
       })
   
       const result = await res.json()
-      setShowCounter('none');
+      setShowCounter('hidden');
     } catch(err) {
       toast.notify('Não foi possível realizar a requisição!', {
         duration: 5,
@@ -54,26 +55,38 @@ export default function IndexPage({
 
 
   return (
-    <Layout>
-      <div className={styles.container}>
-        <Head>
-          <title>Medium App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <ToastContainer />
-        <main className={styles.main}>
-          <div className={styles.clapper}>
+    <StyleRoot>
+      <Layout>
+        <div className={styles.container}>
+          <Head>
+            <title>Medium App</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <ToastContainer />
+          <main className={styles.main}>
+            <div id="clap" className={styles.clapper} style={animate ? styles1.pulse : undefined}>
             <div className={styles.clapCounter} 
-              style={{display: `${showCounter}`}}> +{clapData?.claps}</div>
-            <button className={styles.clap} onClick={handleClaps}>
-              {"👏"}
-            </button>
-            <span><h2>{clapData?.claps} claps</h2></span>
-          </div>
-        </main>
-      </div>
-    </Layout>
+                style={{visibility: `${showCounter}`}}> +{clapData?.claps}</div>
+              <button className={styles.clap} onClick={handleClaps}>
+                {"👏"}
+              </button>
+              <span><h2>{clapData?.claps} claps</h2></span>
+            </div>
+          </main>
+        </div>
+      </Layout>
+    </StyleRoot>
   )
+}
+const styles1 = {
+  pulse: {
+    animation: 'x 100ms',
+    animationName: Radium.keyframes(pulse, 'pulse')
+  },
+  fadeIn: {
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  }
 }
 
 export async function getServerSideProps() {
